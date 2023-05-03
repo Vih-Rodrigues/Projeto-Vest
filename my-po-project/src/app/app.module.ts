a6d2d0ff672649b367cb4bbabd4dfa297bafe53f
Patricia Ferreira <pferreira0925@gmail.com>
2023-05-02 21:46:07 -0300

    Add Prod - Start FrontEnd

---
+++ my-po-project/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PoModule } from '@po-ui/ng-components';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PoModule,
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    PoTemplatesModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
